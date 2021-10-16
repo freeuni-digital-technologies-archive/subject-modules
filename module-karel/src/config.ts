d1d1aab25660b6bebf0c470bbb456dde3e67acd0
iarigby <marvinzem@gmail.com>
2021-10-16 16:57:09 +0200

    add cli argument

--- module-karel/src/config.ts
+++ module-karel/src/config.ts
@@ -39,6 +39,7 @@ export function getArgs(): EnvOptions {
     parser.addArgument(['-f', '--force'], {help: 'force check of id'})
     parser.addArgument(['-k', '--skip'], {help: 'skip check of id'})
     parser.addArgument(['-l', '--late'], {help: 'ignore late of id'})
+    parser.addArgument(['-p', '--config-path'], {help: 'homework config path'})
     const args = parser.parseArgs()
     const hwId: string = args['hw']
 
