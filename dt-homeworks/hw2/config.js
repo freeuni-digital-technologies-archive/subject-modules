a407f2beb1939d4f494cbdd2ccee2d92a1980901
auttenbergo <gigi.gvaramia@gmail.com>
2021-10-16 19:22:43 +0400

    added configuration folders

---
+++ dt-homeworks/hw2/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    id: 'hw2',
+    classroomName: 'დავალება 2',
+    deadline: '2021-10-14',
+    testFileName: 'hw2tester.js',
+    emailTemplate(){
+            return "HW2";
+    }
+}   
