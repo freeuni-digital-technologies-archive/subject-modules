ae3c790f95bf50e4d2bcc6f86f629fbae61e658c
auttenbergo <gigi.gvaramia@gmail.com>
2021-10-24 20:56:19 +0400

    added submissionsAndResults.js

---
+++ module-karel/test/files/integrationTest/submissionsAndResults.js
@@ -0,0 +1,31 @@
+module.exports.submissions = [
+
+]
+
+module.exports.results = {
+    passed: {
+
+    },
+    failed: {
+
+    },
+    late: {
+
+    },
+    crashed: {
+
+    },
+    invalid: {
+
+    },
+    error: {
+
+    },
+    notSubmitted: {
+
+    },
+
+    none: {
+
+    }
+}
