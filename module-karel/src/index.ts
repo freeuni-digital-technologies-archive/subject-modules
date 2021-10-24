0090c0806cb34439c7c4a55bc78805f1285f5625
auttenbergo <gigi.gvaramia@gmail.com>
2021-10-24 18:13:43 +0400

    index.ts main func decomposition, excluding outter functionality, tested for hw1 and hw2

--- module-karel/src/index.ts
+++ module-karel/src/index.ts
@@ -1,21 +1,20 @@
-import { setEnv, testerPath, config } from './config'
-import { getSubmissions, createDrive } from 'classroom-api'
-import { Run, log } from './runs'
+import { setEnv, config } from './config'
+const { hw, runOpts } = setEnv()
+import { getSubmissions } from 'classroom-api'
+import { Run } from './runs'
 import { partitionResults } from './partitions'
 
-import { downloadAndTest } from "./indexHelper";
+import {sliceSubmissions, filterSubmissions, logDownloadingSubmissions, finishSubmissions } from "./integration";
 
-const { hw, slice, runOpts } = setEnv()
 const run = new Run(hw, runOpts)
 
 async function main() {
-    const drive = await createDrive()
     const submissions = await getSubmissions(config.subject, hw.name)
-        .then(submissions => slice ? submissions.slice(0, slice) : submissions)
-        .then(submissions => submissions
-            .filter(s => !hw.skip?.includes(s.emailId) && (run.forceCheck(s) || run.newSubmission(s))))
-        .then(s => log(s, `downloading ${s.filter(e => e.onTime()).length}`))
-        .then(submissions => submissions.filter(s=>s.attachment!=undefined).map((s, i) => downloadAndTest(s, drive, i)))
+        .then(sliceSubmissions)
+        .then(filterSubmissions)
+        .then(logDownloadingSubmissions)
+        .then(finishSubmissions)
+
     const results = await Promise.all(submissions)
     const output = partitionResults(results, hw)
 
--- module-karel/src/integration.ts
+++ module-karel/src/integration.ts
@@ -1,15 +1,23 @@
 
 import { setEnv, testerPath, config } from "./config";
-import { Submission, Drive, saveFile } from "classroom-api";
+import { Submission, Drive, saveFile, createDrive } from "classroom-api";
 import { Run, log } from "./runs";
 import { Result, testSubmission } from "codehskarel-tester";
 
-const { hw, download, runOpts } = setEnv();
+const { hw, slice, download, runOpts } = setEnv();
 
 const testPath = testerPath(hw.id);
 const run = new Run(hw, runOpts)
 
-export function downloadAndTest(submission: Submission, drive: Drive, index: number): Promise<Submission> {
+
+/*
+    Downloads submission and tests it.
+    It finds test file in 'testerPath' path.
+    It also logs if the file has been downloaded or has been tested
+
+    Returns the result for the current submission, if any error occurs, catches it and logs it too
+*/
+function downloadAndTest(submission: Submission, drive: Drive, index: number): Promise<Submission> {
     if (!run.forceCheck(submission) && !submission.qualifies()) {
         return new Promise(r => r(submission))
     }
@@ -22,6 +30,11 @@ export function downloadAndTest(submission: Submission, drive: Drive, index: num
         .catch((error: any) => logError(submission, error))
 }
 
+/*
+    This is used for downloading and saving the submission.
+    Saving path is included.
+
+*/
 function downloadAtInterval(submission: Submission, drive: Drive,  index: number): Promise<string> {
     const attachment = submission.attachment!
     const fileName = attachment.title
@@ -60,7 +73,9 @@ function downloadAtInterval(submission: Submission, drive: Drive,  index: number
 //     })
 // }
 
-
+/*
+    Simply: logs the given error
+*/
 function logError(submission: Submission, error: any) {
     submission.results.push({
         error: true,
@@ -70,4 +85,64 @@ function logError(submission: Submission, error: any) {
     log({}, `error: ${submission.emailId}, ${error}`)
     submission.crashed = true
     return submission
+}
+
+
+/*
+    DECOMPOSITION SECTION
+
+    src: index.ts -> function main()
+
+    Divided by 4 steps
+    
+*/
+
+
+/*
+    Step 1) 
+        Slicing submissions after getting them from classrom-api module
+*/
+export function sliceSubmissions(submissions: Submission[]){
+    return slice ? submissions.slice(0,slice) : submissions;
+}
+
+/*
+    Step 2) 
+        Filtering sliced submissions for further operations
+*/
+export function filterSubmissions(submissions: Submission[]){
+    return submissions.filter(
+        s => !hw.skip?.includes(s.emailId) && (run.forceCheck(s) || run.newSubmission(s))
+    );
+}
+
+/*
+    Step 3) 
+        Log submissions after filtering
+*/
+
+export function logDownloadingSubmissions(submissions: Submission[]){
+    //log(s, `downloading ${s.filter(e => e.onTime()).length}`
+
+    const text = submissions.filter(submission => {
+        submission.onTime();
+    }).length
+
+    return log(submissions,`downloading ${text}`);
+}
+
+/*
+    Step 4) 
+        Validate submissions with attachments, download and test them
+*/
+
+export async function finishSubmissions(submissions: Submission[]){
+    const drive = await createDrive();
+    let submissionsWithAttachments: Submission[] = submissions.filter(submission => {
+        return submission.attachment != undefined;
+    });
+    
+    return submissionsWithAttachments.map((submission, index) => {
+        return downloadAndTest(submission,drive, index)
+    });
 }
