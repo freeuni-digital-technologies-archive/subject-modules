bfe3829af4af20a4c76bd857c011eccbca7c5c90
auttenbergo <gigi.gvaramia@gmail.com>
2021-11-13 00:07:22 +0400

    homework data and test files

--- module-karel/test/files/integrationTest/submissionsAndResults.js
+++ module-karel/test/files/integrationTest/submissionsAndResults.js
@@ -1,14 +1,203 @@
 module.exports.submissions = [
+    {
+        "id": "Cg4IqsaL2uALEOewj9nlCw",
+        "emailId": "tkoba21",
+        "emailAddress": "tkoba21@freeuni.edu.ge",
+        "state": "TURNED_IN",
+        "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDA1MjU2MDAxNjM5/submissions/by-status/and-sort-last-name/student/NDAzOTE1ODU4NzMw",
+        "attachment": {
+                "id": "1jR3KUYDk-QMYymCJMre7fkeu6YA7jC5i",
+                "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1jR3KUYDk-QMYymCJMre7fkeu6YA7jC5i&export=download",
+                "title": "tkoba21.txt..txt"
+        },
+        "timeStamp": "2021-10-07T16:49:30.135Z"
+    },
+    {
+            "id": "Cg4IuMTD6eALEOewj9nlCw",
+            "emailId": "nchil21",
+            "emailAddress": "nchil21@freeuni.edu.ge",
+            "state": "TURNED_IN",
+            "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDA1MjU2MDAxNjM5/submissions/by-status/and-sort-last-name/student/NDAzOTQ4MjMzMjcy",
+            "attachment": {
+                    "id": "1GF2F-HZe9MLQ74zTPtNzrAmfqzAjY10N",
+                    "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1GF2F-HZe9MLQ74zTPtNzrAmfqzAjY10N&export=download",
+                    "title": "nchil21.txt.txt"
+            },
+            "timeStamp": "2021-10-06T19:26:42.419Z"
+    },
+    {
+        "id": "Cg4Ip7Hh5K0LEOewj9nlCw",
+        "emailId": "mkhor21",
+        "emailAddress": "mkhor21@freeuni.edu.ge",
+        "state": "TURNED_IN",
+        "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDA1MjU2MDAxNjM5/submissions/by-status/and-sort-last-name/student/MzkwMjQ4MDI4MzI3",
+        "attachment": {
+                "id": "1ulO7_vrznAYKR9M_fe0mhfuLkjG76Xcd",
+                "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1ulO7_vrznAYKR9M_fe0mhfuLkjG76Xcd&export=download",
+                "title": "Your_First_Karel_Program.txt"
+        },
+        "timeStamp": "2021-10-06T20:28:20.052Z"
+    },
+    {
+        "id": "Cg4InsvF9OALEOewj9nlCw",
+        "emailId": "sjorj21",
+        "emailAddress": "sjorj21@freeuni.edu.ge",
+        "state": "TURNED_IN",
+        "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDA1MjU2MDAxNjM5/submissions/by-status/and-sort-last-name/student/NDAzOTcxMzM1NTgy",
+        "attachment": {
+                "id": "1WVOexaQ6VsArfQUdjHmdSYmLbzB_x4bs",
+                "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1WVOexaQ6VsArfQUdjHmdSYmLbzB_x4bs&export=download",
+                "title": "sjorj.txt..txt"
+        },
+        "timeStamp": "2021-10-06T19:16:20.331Z"
+    },
+    {
+        "id": "Cg4IqLmE4dgLEPPJ4djZCw",
+        "emailId": "adane20",
+        "emailAddress": "adane20@freeuni.edu.ge",
+        "state": "TURNED_IN",
+        "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDAyMDM0MDI1NzE1/submissions/by-status/and-sort-last-name/student/NDAxNzgyOTM4Nzky",
+        "attachment": {
+                "id": "16XCi_oqNhUQNSW7iIYrq-ZP7JyBaTHdI",
+                "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=16XCi_oqNhUQNSW7iIYrq-ZP7JyBaTHdI&export=download",
+                "title": "adane20_dav2.txt"
+        },
+        "timeStamp": "2021-10-10T19:38:24.792Z"
+    },
+    {
+        "id": "Cg4I65-e2-ALEPPJ4djZCw",
+        "emailId": "mtigi21",
+        "emailAddress": "mtigi21@freeuni.edu.ge",
+        "state": "TURNED_IN",
+        "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDAyMDM0MDI1NzE1/submissions/by-status/and-sort-last-name/student/NDAzOTE4MjYyMjUx",
+        "attachment": {
+                "id": "1YzLc75kZzUrbauaQ-3zRpp8A2RRHC-1c",
+                "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1YzLc75kZzUrbauaQ-3zRpp8A2RRHC-1c&export=download",
+                "title": "mtigi21.txt.Pancakes.txt"
+        },
+        "timeStamp": "2021-10-11T10:10:49.921Z"
+    }
+
 
 ]
 
 module.exports.results = {
-    passed: {
-
+    /* FROM HW1 */
+    passed: [
+        {
+            "id": "Cg4IqsaL2uALEOewj9nlCw",
+            "emailId": "tkoba21",
+            "emailAddress": "tkoba21@freeuni.edu.ge",
+            "state": "TURNED_IN",
+            "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDA1MjU2MDAxNjM5/submissions/by-status/and-sort-last-name/student/NDAzOTE1ODU4NzMw",
+            "results": [
+                    {
+                            "passed": true,
+                            "message": "კარელმა უნდა აიღოს ბურთი რომელიც 5x1-ზე დევს. Karel is on position (5,1), coordinates of beepers: , world: 10x10"
+                    },
+                    {
+                            "passed": true,
+                            "message": "კარელი უნდა იყოს 5x1 პოზიციაზე"
+                    }
+            ],
+            "attachment": {
+                    "id": "1jR3KUYDk-QMYymCJMre7fkeu6YA7jC5i",
+                    "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1jR3KUYDk-QMYymCJMre7fkeu6YA7jC5i&export=download",
+                    "title": "tkoba21.txt..txt"
+            },
+            "timeStamp": "2021-10-07T16:49:30.135Z"
     },
-    failed: {
+    {
+            "id": "Cg4IuMTD6eALEOewj9nlCw",
+            "emailId": "nchil21",
+            "emailAddress": "nchil21@freeuni.edu.ge",
+            "state": "TURNED_IN",
+            "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDA1MjU2MDAxNjM5/submissions/by-status/and-sort-last-name/student/NDAzOTQ4MjMzMjcy",
+            "results": [
+                    {
+                            "passed": true,
+                            "message": "კარელმა უნდა აიღოს ბურთი რომელიც 5x1-ზე დევს. Karel is on position (5,1), coordinates of beepers: , world: 10x10"
+                    },
+                    {
+                            "passed": true,
+                            "message": "კარელი უნდა იყოს 5x1 პოზიციაზე"
+                    }
+            ],
+            "attachment": {
+                    "id": "1GF2F-HZe9MLQ74zTPtNzrAmfqzAjY10N",
+                    "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1GF2F-HZe9MLQ74zTPtNzrAmfqzAjY10N&export=download",
+                    "title": "nchil21.txt.txt"
+            },
+            "timeStamp": "2021-10-06T19:26:42.419Z"
+    }
+    ],
 
+    /* FROM HW2 */
+    failed: [
+        {
+            "id": "Cg4IqLmE4dgLEPPJ4djZCw",
+            "emailId": "adane20",
+            "emailAddress": "adane20@freeuni.edu.ge",
+            "state": "TURNED_IN",
+            "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDAyMDM0MDI1NzE1/submissions/by-status/and-sort-last-name/student/NDAxNzgyOTM4Nzky",
+            "results": [
+                    {
+                            "passed": false,
+                            "message": "კარელი უნდა იდგეს 8x1: expected { x: 10, y: 10 } to deeply equal { x: 8, y: 1 }"
+                    },
+                    {
+                            "passed": false,
+                            "message": "კარელი უნდა იდგეს 11x1: expected { x: 10, y: 10 } to deeply equal { x: 11, y: 1 }"
+                    },
+                    {
+                            "passed": false,
+                            "message": "კარელი უნდა იდგეს 1x1: expected { x: 10, y: 10 } to deeply equal { x: 1, y: 1 }"
+                    },
+                    {
+                            "passed": false,
+                            "message": "კარელი უნდა იდგეს 6x1: expected { x: 10, y: 10 } to deeply equal { x: 6, y: 1 }"
+                    }
+            ],
+            "attachment": {
+                    "id": "16XCi_oqNhUQNSW7iIYrq-ZP7JyBaTHdI",
+                    "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=16XCi_oqNhUQNSW7iIYrq-ZP7JyBaTHdI&export=download",
+                    "title": "adane20_dav2.txt"
+            },
+            "timeStamp": "2021-10-10T19:38:24.792Z"
     },
+    {
+            "id": "Cg4I65-e2-ALEPPJ4djZCw",
+            "emailId": "mtigi21",
+            "emailAddress": "mtigi21@freeuni.edu.ge",
+            "state": "TURNED_IN",
+            "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDAyMDM0MDI1NzE1/submissions/by-status/and-sort-last-name/student/NDAzOTE4MjYyMjUx",
+            "results": [
+                    {
+                            "passed": false,
+                            "message": "კარელი უნდა იდგეს 2x1: expected { x: 7, y: 1 } to deeply equal { x: 2, y: 1 }"
+                    },
+                    {
+                            "passed": false,
+                            "message": "კარელი უნდა იდგეს 2x1: expected { x: 7, y: 1 } to deeply equal { x: 2, y: 1 }"
+                    },
+                    {
+                            "passed": false,
+                            "message": "კარელი უნდა იდგეს 1x1: expected { x: 7, y: 1 } to deeply equal { x: 1, y: 1 }"
+                    },
+                    {
+                            "passed": false,
+                            "message": "კარელი უნდა იდგეს 2x1: expected { x: 7, y: 1 } to deeply equal { x: 2, y: 1 }"
+                    }
+            ],
+            "attachment": {
+                    "id": "1YzLc75kZzUrbauaQ-3zRpp8A2RRHC-1c",
+                    "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1YzLc75kZzUrbauaQ-3zRpp8A2RRHC-1c&export=download",
+                    "title": "mtigi21.txt.Pancakes.txt"
+            },
+            "timeStamp": "2021-10-11T10:10:49.921Z"
+    }
+
+    ],
     late: {
 
     },
@@ -18,9 +207,50 @@ module.exports.results = {
     invalid: {
 
     },
-    error: {
+    /* FROM HW1 */
+    error: [
+{
+                "id": "Cg4Ip7Hh5K0LEOewj9nlCw",
+                "emailId": "mkhor21",
+                "emailAddress": "mkhor21@freeuni.edu.ge",
+                "state": "TURNED_IN",
+                "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDA1MjU2MDAxNjM5/submissions/by-status/and-sort-last-name/student/MzkwMjQ4MDI4MzI3",
+                "results": [
+                        {
+                                "error": true,
+                                "message": "there is a problem with the file",
+                                "details": "Unknown error occured: move is not defined, move is not defined"
+                        }
+                ],
+                "attachment": {
+                        "id": "1ulO7_vrznAYKR9M_fe0mhfuLkjG76Xcd",
+                        "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1ulO7_vrznAYKR9M_fe0mhfuLkjG76Xcd&export=download",
+                        "title": "Your_First_Karel_Program.txt"
+                },
+                "timeStamp": "2021-10-06T20:28:20.052Z"
+        },
+        {
+                "id": "Cg4InsvF9OALEOewj9nlCw",
+                "emailId": "sjorj21",
+                "emailAddress": "sjorj21@freeuni.edu.ge",
+                "state": "TURNED_IN",
+                "alternateLink": "https://classroom.google.com/c/Mzk5NzYzODYyMjQz/a/NDA1MjU2MDAxNjM5/submissions/by-status/and-sort-last-name/student/NDAzOTcxMzM1NTgy",
+                "results": [
+                        {
+                                "error": true,
+                                "message": "there is a problem with the file",
+                                "details": "Unknown error occured: move is not defined, move is not defined"
+                        }
+                ],
+                "attachment": {
+                        "id": "1WVOexaQ6VsArfQUdjHmdSYmLbzB_x4bs",
+                        "downloadUrl": "https://drive.google.com/a/freeuni.edu.ge/uc?authuser=0&id=1WVOexaQ6VsArfQUdjHmdSYmLbzB_x4bs&export=download",
+                        "title": "sjorj.txt..txt"
+                },
+                "timeStamp": "2021-10-06T19:16:20.331Z"
+        }
 
-    },
+    ],
     notSubmitted: {
 
     },
