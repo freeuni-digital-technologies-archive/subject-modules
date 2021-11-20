e7b139ccbb0cde92f8327cfa25dfa0bbfe72f999
iarigby <marvinzem@gmail.com>
2021-11-20 12:31:23 +0400

    extract email bodies to use in reminder messages

--- module-karel/src/templates.ts
+++ module-karel/src/templates.ts
@@ -3,7 +3,7 @@ import { Submission, getStudentByEmail } from "classroom-api";
 import { getArgs } from './cli'
 const {hw} = getArgs()
 
-type S = Submission
+export type S = Submission
 const urls = {
 	homework: 'https://freeuni-digital-technologies.github.io/homework/',
     web_homework: 'https://freeuni-digital-technologies.github.io/homework/web_hws.html'
@@ -28,7 +28,7 @@ const blocked = 'ამ პრობლემის გამო დავალ
 
 export const templates: Partitions<(s: S) => string> | any = {
     late: (s: S) => `
-        გამარჯობა ${getStudentByEmail(s.emailId)?.georgianName},
+        ${summaries.greeting(s)},
 
         დავალება დაგვიანებით ატვირთე და ქულა არ ჩაგეთვლება, მაგრამ უკუკავშირის მიზნით გიგზავნი შედეგს:
 
@@ -39,44 +39,21 @@ export const templates: Partitions<(s: S) => string> | any = {
         ${fileInfo(s)}
     `,
     invalid: (s: S) => `
-        გამარჯობა ${getStudentByEmail(s.emailId)?.georgianName},
+        ${summaries.greeting(s)},
 
-        როგორც ჩანს, დავალება ადრე დაიწყე და ჩემს მიერ არასწორად გამოგზავნილ წესს მიყევი ატვირთვისას.
-        პროგრამამ რომ შენი დავალების გასწორება შეძლოს, საჭიროა განახლებული წესებით ატვირთო. 
-         ფაილები დაზიპე და ასევე
-        html ფაილს აუცილებლად index.html უნდა ერქვას. ბოდიშს გიხდი ამ შეცდომის გამო.
+        ${summaries.invalid(s)}
 
-        დაზიპვა თუ არ იცი, ინსტრუქცია ამ გვერდზეა: ${urls.web_homework}. თუ თავიდან ატვირთვის
-        შემდეგ პრობლემა ისევ განმეორდა, დაუკავშირდი გიგის რომ დაგეხმაროს <3 
-
-        
-        დიდი მადლობა!
         ია
-
-        ${s.results}
-
         
         ${fileInfo(s)}
     `,
 
     // error/invalid მესიჯები უნდა იყოს ერთგან გაწერილი და ყველას თავისი მესიჯი/გამოსწორება ეწეროს
     error: (s: S) => `
-        გამარჯობა ${getStudentByEmail(s.emailId)?.georgianName},
+        ${summaries.greeting(s)},
 
+        ${summaries.error(s)}
         
-        ფაილის წესებიც სრულყოფილად გაქვს დაცული, მაგრამ 
-        სინტაქსური ან სხვა პრობლემის გამო პროგრამა კოდს ვერ უშვებს. რადგანაც
-        პროგრამის დიდი ნაწილი ახალი შეცვლილია, დიდი შანსია ჩვენი შეცდომაც იყოს.
-        მიწერე გიგის და გაარკვევთ ერთად <3
-
-        მეილს თან ვურთავ ტესტერის მესიჯს.
-        
-        ${urls.homework}
-
-        ${s.results}
-
-        ${blocked}
-
         ია
         
         ${fileInfo(s)}
@@ -84,7 +61,7 @@ export const templates: Partitions<(s: S) => string> | any = {
     // TODO პირველი/მეორე და ა.შ ქონდეს დავალებასაც კონფიგურაციაში,
     // ასევე ლინკი საიტის.
     failed: (s: S) => `
-        გამარჯობა ${getStudentByEmail(s.emailId)?.georgianName},
+        ${summaries.greeting(s)},
         
         ფაილის წესები სრულყოფილად გაქვს დაცული და კოდშიც
         სინტაქსური შეცდომები არაა, მაგრამ შენი კოდი ზოგ ტესტს არ გადის. 
@@ -101,7 +78,7 @@ export const templates: Partitions<(s: S) => string> | any = {
         ${fileInfo(s)}
     `,
     passed: (s: S) => `
-        გამარჯობა ${getStudentByEmail(s.emailId)?.georgianName},
+        ${summaries.greeting(s)},
 
 	   დავალება ჩაბარებულია. ყველაფერი სწორად გააკეთე :) 
 
@@ -109,3 +86,47 @@ export const templates: Partitions<(s: S) => string> | any = {
     `
 
 }
+
+
+export const summaries = {
+    greeting: (s: S) => {
+    return `გამარჯობა ${getStudentByEmail(s.emailId)?.georgianName}`
+    },
+
+    error: (s: S) => {
+        return `
+            ფაილის წესებიც სრულყოფილად გაქვს დაცული, მაგრამ 
+            სინტაქსური ან სხვა პრობლემის გამო პროგრამა კოდს ვერ უშვებს. რადგანაც
+            პროგრამის დიდი ნაწილი ახალი შეცვლილია, დიდი შანსია ჩვენი შეცდომაც იყოს.
+            მიწერე გიგის და გაარკვევთ ერთად <3
+
+            მეილს თან ვურთავ ტესტერის მესიჯს.
+            
+            ${urls.homework}
+
+            ${s.results}
+
+            ${blocked}
+        `
+    },
+
+    invalid: (s: S) => {
+        return `
+            როგორც ჩანს, დავალება ადრე დაიწყე და ჩემს მიერ არასწორად გამოგზავნილ წესს მიყევი ატვირთვისას.
+            პროგრამამ რომ შენი დავალების გასწორება შეძლოს, საჭიროა განახლებული წესებით ატვირთო. 
+             ფაილები დაზიპე და ასევე
+            html ფაილს აუცილებლად index.html უნდა ერქვას. ბოდიშს გიხდი ამ შეცდომის გამო.
+
+            დაზიპვა თუ არ იცი, ინსტრუქცია ამ გვერდზეა: ${urls.web_homework}. თუ თავიდან ატვირთვის
+            შემდეგ პრობლემა ისევ განმეორდა, დაუკავშირდი გიგის რომ დაგეხმაროს <3 
+
+            
+            დიდი მადლობა!
+
+            ${s.results}
+    `
+
+    }
+
+}
+
