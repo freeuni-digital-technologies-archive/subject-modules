d2017981dde6d7a60cda4b6a7d17aabab477c7bc
iarigby <marvinzem@gmail.com>
2021-11-22 01:26:57 +0400

    bleh

--- dt-homeworks/hw5/config.js
+++ dt-homeworks/hw5/config.js
@@ -2,7 +2,7 @@ module.exports = {
     id: 'hw5',
     classroomName: 'დავალება 5',
     deadline: '2021-11-21',
-    testFileName: '../../webHwInterface/server-tests/hw5/server.js',
+    testFileName: '../../../webHwInterface/server-tests/hw5/server.js',
     module: 'web',
     emailTemplate(){
             return "HW2";
