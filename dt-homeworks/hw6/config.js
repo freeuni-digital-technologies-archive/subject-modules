9eb6dc541408b15fc7a33a255c5e3d549eab3b19
iarigby <marvinzem@gmail.com>
2021-11-23 16:21:06 +0400

    add homework 6

---
+++ dt-homeworks/hw6/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    id: 'hw6',
+    classroomName: 'დავალება 6',
+    deadline: '2021-12-02',
+    testFileName: '../../../webHwInterface/server-tests/hw6/server.js',
+    module: 'web',
+    emailTemplate(){
+            return "HW2";
+    } 
+}   
