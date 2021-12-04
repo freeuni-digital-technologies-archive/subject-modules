cb4deb4decd81983d2e3651b93571e19f3f6f953
iarigby <marvinzem@gmail.com>
2021-12-04 17:02:29 +0400

    prepare types

--- module-karel/src/module.ts
+++ module-karel/src/module.ts
@@ -1,14 +1,18 @@
-import { Submission, Drive } from "dt-types";
+import { Submission, Drive } from 'dt-types';
 import { Run, log } from "./runs";
 // TODO dt-types
 import { Result } from 'website-tester'
+import { Partitions } from "./partitions";
+import {  EmailTemplate } from './templates'
+
 
 export interface SubjectModule {
 	downloadAtInterval: (submission: Submission, drive: Drive,  index: number, run: Run, saveFile: any) => Promise<string>,
 	// ამ ორის არგუმენტები ძაან მაგარია... ჩემი შემოქმედება
 	testSubmission: (testPath: string, path: string) => Promise<Result[]>,
 	prepareSubmission: (path: string, testPath: string) => string,
-	asynchronousTest: boolean
+	asynchronousTest: boolean,
+	emailTemplates?: Partitions<EmailTemplate>
 }
 
 export function defaultPrepareSubmission(path: string, testPath: string): string {
